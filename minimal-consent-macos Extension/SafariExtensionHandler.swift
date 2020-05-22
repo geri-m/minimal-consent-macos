8396468cfa76ede264ba93aba4b9ba1247b2cf23
Gerald Madlmayr <gerald.madlmayr@gmx.at>
2020-05-22 10:34:30 +0200

    Update JavaScript, Update Backend Call

--- minimal-consent-macos Extension/SafariExtensionHandler.swift
+++ minimal-consent-macos Extension/SafariExtensionHandler.swift
@@ -23,26 +23,25 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
         // This method will be called when a content script provided by your extension calls safari.extension.dispatchMessage("message").
         page.getPropertiesWithCompletionHandler { properties in
             NSLog("The extension received a message (\(messageName)) from a script injected into (\(String(describing: properties?.url))) with userInfo (\(userInfo ?? [:]))")
-            
+           
         }
         
+        // get the page properties and
+        page.getPropertiesWithCompletionHandler { properties in self.postStatusToBackend(with: properties, userInfo: userInfo) }
+    }
         
-        /*
-         The extension received a message (someMessage) from a script injected into (Optional(https://www.zusammengegencorona.de/)) with userInfo (["from": backendCall, "implemented": 1, "cmp": CookieBot, "cmpScripUrl": https://consent.cookiebot.com/uc.js?cbid=79294954-59b2-4580-b868-a79cc7b61270, "pingResult": {
-         cmpId = 134;
-         }])
-         */
+    func postStatusToBackend(with prop: SFSafariPageProperties?, userInfo: [String : Any]? ){
         
         var newDict = self.unimmutable(dict:userInfo!)
         
         //  let he = new HistoryEntry(now, link.host, request.cmp, request.cmpScripUrl, pr, request.implemented);
         
-        // Unwrapping the userInfo
-        newDict["url"] = "www.orf.at"
-        newDict["date"] = "2020-05-19 12:00:00"
-        newDict["version"] = "0.0.0"
-        newDict["uuid"]  = "some uuid from OSX"
+        NSLog("Current URL: " + (prop?.url?.absoluteString)!)
         
+        // Unwrapping the userInfo
+        newDict["url"] = prop?.url?.absoluteString
+        newDict["version"] = "1.0.7"
+        newDict["uuid"]  = "some-uuid-from-OSX"
         
         NSLog("User Info: (\(userInfo ?? [:]))")
         
@@ -67,7 +66,7 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
         request.addValue("application/json", forHTTPHeaderField: "Accept")
         
         //create dataTask using the session object to send data to the server
-        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+        let task = session.dataTask(with: request as URLRequest/*, completionHandler: { data, response, error in
             
             guard error == nil else {
                 return
@@ -86,74 +85,28 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
             } catch let error {
                 NSLog("Data:" + error.localizedDescription)
             }
-        })
+        }*/)
         task.resume()
         
         NSLog("Done sending")
-        
-        
     }
     
-    /**
-     private async handleBackendCall(request: any, sender: any, sendResponse: any) {
-     Utils.log("Handle: " + BackendCall.pageName);
-     let link = await this.getUrl();
-     
-     // check, if we have already something in the local storage.
-     let lastFound = await this._history.getLastFound(link.host);
-     
-     // if there is already something, don't process further.
-     if (lastFound !== null) {
-     Utils.log("The Page is already in the History. Don't consider further");
-     }
-     // only HTTP Pages will be supported
-     else if (link.isHttp) {
-     if (request.cmp && request.cmpScripUrl && typeof request.pingResult !== 'undefined' && typeof request.implemented !== 'undefined') {
-     let pr = PingResult.class(request.pingResult);
-     
-     Utils.log("Ping Result: " + JSON.stringify(pr));
-     // for Security Reasons, we pass each Element separably over to the insert Method.
-     let now = dateFormat(new Date(), 'yyyy-mm-dd HH:MM:ss');
-     let he = new HistoryEntry(now, link.host, request.cmp, request.cmpScripUrl, pr, request.implemented);
-     let uuid = await this._deviceId.loadOrGenerate();
-     this.logBackend(he, uuid.deviceId, chrome.runtime.getManifest().version);
-     this.switchIcon(he.implemented);
-     this.storeRequest(he);
-     }
-     } else {
-     Utils.log("handleBackendCall: Current Page is not HTTP/HTTPS");
-     }
-     }
-     
-     ------
-     
-     public send(requestJson: { [id: string]: any }, uuid: string, version: string): void {
-     // adding Version to Request. As the parameters was an object, it looks like we can not modify it .. wired.
-     let temp = JSON.parse(JSON.stringify(requestJson));
-     temp.version = version;
-     temp.uuid = uuid;
-     this.xhr.open(Request.HTTP_METHOD, Request.URL_CONSENT, true);
-     //Send the proper header information along with the request
-     this.xhr.setRequestHeader("Content-Type", "application/json");
-     // Sanity Check, so we only send correct data to the backend.
-     this.xhr.send(JSON.stringify(temp));
-     Utils.log("Backend call done:" + JSON.stringify(temp));
-     }
-     
-        */
-    
+    /*
     override func toolbarItemClicked(in window: SFSafariWindow) {
         // This method will be called when your toolbar item is clicked.
         NSLog("The extension's toolbar item was clicked")
     }
-    
+    */
+ 
     override func validateToolbarItem(in window: SFSafariWindow, validationHandler: @escaping ((Bool, String) -> Void)) {
         // This is called when Safari's state changed in some way that would require the extension's toolbar item to be validated again.
         validationHandler(true, "")
     }
     
+    /*
     override func popoverViewController() -> SFSafariExtensionViewController {
         return SafariExtensionViewController.shared
     }
+     */
     
 }
