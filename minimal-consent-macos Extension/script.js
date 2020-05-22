5adefd12a8fdce637a3f72c49faa9ca026b3d57d
Gerald Madlmayr <gerald.madlmayr@gmx.at>
2020-05-22 11:02:22 +0200

    Release 1.0

--- minimal-consent-macos Extension/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    safari.extension.dispatchMessage("Hello World!");
-    console.log("Document:" + document);
-    console.log("Document Body:"  + document.body);
-    console.log("Document Body:"  + document.body.innerHTML.length);
-    console.log("Document Body:"  + document.getElementsByTagName("body").innerHTML.length);
-});
